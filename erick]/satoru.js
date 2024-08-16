f3e2f1966293f612310c893ee77bc52cf8dba0b4
erick <162378167+erickgojo@users.noreply.github.com>
2024-08-16 17:53:40 -0300

    ftrgrdhy

---
+++ erick]/satoru.js
@@ -0,0 +1,475 @@
+const cartas = [
+  {
+    "nome": "Mountanha",
+    "tipo": "Terreno",
+    "forca": "0",
+    "resistencia": "0",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "275",
+    "quantidade": "8",
+    "imagem_url": "URL da Imagem",
+    "habilidades": []
+  },
+
+  {
+    "nome": "floresta",
+    "tipo": "Terreno",
+    "forca": "0",
+    "resistencia": "0",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "277",
+    "quantidade": "8",
+    "imagem_url": "URL da Imagem",
+    "habilidades": []
+  },
+
+  {
+    "nome": "Batalhão Skitterbeam",
+    "tipo": "Terreno",
+    "valor de mana": "9",
+    "custo_mana": "9",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "Mitico Raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "165",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Atropelar, pressa, Protótipo"
+
+      }
+    ]
+  },
+  {
+    "nome": "Amálgama Rootwire",
+    "tipo": "Criatura Artefato - Golem",
+    "valor de mana": "5",
+    "custo_mana": "5",
+    "forca": "5",
+    "resistencia": "5",
+    "raridade": "Mitico Raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "203",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Sacrifique Rootwire Amalgam, Protótipo"
+        
+      }
+    ]
+  },
+
+  
+  {
+    "nome": "Mishra, Excavation Prodigy",
+    "tipo": "Legendary Creature — Human Artificer",
+    "custo_mana": "2",
+    "valor de mana" : "3",
+    "forca": "2",
+    "resistencia": "1",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "140",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Discard a card"
+        
+      }
+    ]
+  },
+  {
+    "nome": "Sarinth Steelseeker",
+    "tipo": "Creature — Human Artificer Scout",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "1",
+    "resistencia": "2",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "189",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Quando um ou mais artefatos entram no campo de batalha sob o seu controle, você pode olhar a carta do topo do seu grimório. Se for uma carta de terreno, você pode revelá-la e colocá-la em sua mão. Se você não colocar a carta na sua mão, pode colocá-la no seu cemitério."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Fallaji Dragon Engine",
+    "tipo": "Artifact Creature — Dragon",
+    "custo_mana": "8",
+    "valor de mana": "8",
+    "forca": "5",
+    "resistencia": "5",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "159",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Voar (Flying), Habilidade de Pump, Habilidade de Pump"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Tawnos's Tinkering",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "191",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Coloque dois marcadores +1/+1 em um artefato, criatura ou terreno alvo que você controla. Desvire aquela permanente. Se ela não for uma criatura, ela se torna uma criatura 0/0 além dos seus outros tipos."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Shoot Down",
+    "tipo": "Sorcery",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "190",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Exile o artefato, encantamento ou criatura com voar alvo."
+  
+      }
+    ]
+  },
+  {
+    "nome": "Bushwhack",
+    "tipo": "Sorcery",
+    "custo_mana": "x",
+    "valor de mana": "1",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "174",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Choose one — Search your library for a basic land card, reveal it, put it into your hand, then shuffle. Target creature you control fights target creature you don’t control. (Each deals damage equal to its power to the other."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Epic Confrontation",
+    "tipo": "sorcery",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "176",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Target creature you control gets +1/+2 until end of turn. It fights target creature you don't control. (Each deals damage equal to its power to the other.)"
+ }
+    ]
+  },
+
+  {
+    "nome": "Mishra's Onslaught",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "143",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "• Create two 1/1 colorless Soldier artifact creature tokens. Creatures you control get +2/+0 until end of turn."
+  
+      }
+    ]
+  },
+  {
+    "nome": "Artificer's Dragon",
+    "tipo": "Artifact Creature — Dragon",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "291",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Flying, Unearth"
+  
+      }
+    ]
+  },
+  {
+    "nome": "Mishra's Juggernaut",
+    "tipo": "Artifact Creature — Juggernaut",
+    "custo_mana": "5",
+    "valor de mana": "5",
+    "forca": "5",
+    "resistencia": "3",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "161",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Trample, Unearth"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Unleash Shell",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "5",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "155",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Unleash Shell deals 5 damage to target creature or planeswalker and 2 damage to that permanent's controller."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Cradle Clearcutter",
+    "tipo": "Artifact Creature — Golem",
+    "custo_mana": "6",
+    "valor de mana": "6",
+    "forca": "3",
+    "resistencia": "6",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "198",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Add an amount of Green equal to Cradle Clearcutter's power, Prototype"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Skitterbeam Battalion",
+    "tipo": "Artifact Creature — Construc",
+    "custo_mana": "9",
+    "valor de mana": "9",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "165",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Trample, haste, Prototype"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Scrapwork Mutt",
+    "tipo": "Artifact Creature — Dog",
+    "custo_mana": "2",
+    "valor de mana": "2",
+    "forca": "2",
+    "resistencia": "1",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "164",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "When Scrapwork Mutt enters, you may discard a card. If you do, draw a card. Unearth"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Scrapwork Mutt",
+    "tipo": "Artifact Creature — Wurm",
+    "custo_mana": "7",
+    "valor de mana": "7",
+    "forca": "4",
+    "resistencia": "6",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "200",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Whenever Iron-Craw Crusher attacks, target attacking creature gets +X/+0 until end of turn, where X is Iron-Craw Crusher's power. Prototype"
+  
+      }
+    ]
+  },
+  {
+    "nome": "Golias de ferrugem",
+    "tipo": "Criatura Artefato - Construir",
+    "custo_mana": "10",
+    "valor de mana": "10",
+    "forca": "10",
+    "resistencia": "10",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "204",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "protótipo (Prototype), Reach, Temple"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Patrulha de perimetro",
+    "tipo": "Criatura - Soldado Humano",
+    "custo_mana": "2",
+    "valor de mana": "3",
+    "forca": "3",
+    "resistencia": "3",
+    "raridade": "Comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "188",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Quando um artefato entra no campo de batalha sob seu controle, a Patrulha do Perimetro ganha +1/+0 até o fim do turno"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Mesa de Pesquisas de Mishra",
+    "tipo": "Artefato",
+    "custo_mana": "1",
+    "valor de mana": "1",
+    "forca": "X",
+    "resistencia": "X",
+    "raridade": "Incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "162",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Sacrifique Mesa de Pesquisas de Mishra: Exile os dois card do topo de seu grimório, Escolha um deles. Até o final do seu próximo turno, você pode jogar aquele card. , Desenterrar"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Automato Chamador de Madeira",
+    "tipo": "Criatura Artefato - Construir",
+    "custo_mana": "10",
+    "valor de mana": "10",
+    "forca": "8",
+    "resistencia": "8",
+    "raridade": "Cru",
+    "colecao": "The Brothers' War",
+    "num_serie": "292",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Prototype, Quando Autmato Chamador de Madeira entra no campo de batalha, se você o conjurar, desvire o terreno que você controle, Ele se tornara numa criatura Treefolk com haste, e poder e defesa base iguais ao poder de Automaton, Ele ainda é um terreno."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Explosão de Escavação",
+    "tipo": "Feitiçaria",
+    "custo_mana": "2",
+    "valor de mana": "3",
+    "forca": "X",
+    "resistencia": "X",
+    "raridade": "Comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "132",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": " Explosão de Escavação causa 3 de dano para qualquer alvo. Crie uma ficha de Powerstone virada"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Sucateiro Tomacul",
+    "tipo": "Criatura - Atifíce Humanp",
+    "custo_mana": "2",
+    "valor de mana": "3",
+    "forca": "2",
+    "resistencia": "1",
+    "raridade": "Comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "153",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Quando Tomakul Scrapmith enrar, triture três card. Você pode colocar um card de artefato dentre os cards triturados dessa forma em sua mão, Se não o fizer, coloque um marcados +1/+1 em Tomakul Scrapsmith. (Para triturar um card, coloque o card do topo do seu grimóreio em seu cemitério."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Caçador de pedras com chifres",
+    "tipo": "Criatura - Lagarto",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "2",
+    "resistencia": "2",
+    "raridade": "Incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "138",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Ameaça"
+  
+      }
+    ]
+  },
+
+  {
+    "nome":"Sprite Argothiano ",
+    "tipo": "Criatura - Fada",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "2",
+    "resistencia": "2",
+    "raridade": "Comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "168",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": " Sprite Argothiano não pode ser bloqueado por criaturas Artefato, Coloque dois marcadores +1/+1 no Sprite Argothiano."
+  
+      }
+    ]
+  }
+
+
+]
+
+console.log(cartas);
